9108aab5dd48a8875c506e7518e6b3cb3b8802d0
Heikki Linnakangas <heikki.linnakangas@iki.fi>
2022-04-14 20:04:36 +0300

    Update to work with rustls 0.20
    Bumped the crate version, because the signature of the ReadHalf and
    WriteHalf structs changed. The change was not backwards-compatible.

--- src/lib.rs
+++ src/lib.rs
@@ -1,51 +1,52 @@
 use std::{
     io,
+    io::Write,
     net::{Shutdown, TcpStream},
     sync::{Arc, Mutex, MutexGuard},
 };
 
-use rustls::Session;
+use rustls::Connection;
 
 mod buffer;
 pub use buffer::BufCfg;
 use buffer::Buffer;
 
-struct Shared<S: Session> {
+struct Shared {
     stream: TcpStream,
-    session: Mutex<S>,
+    connection: Mutex<Connection>,
 }
 
-pub struct ReadHalf<S: Session> {
-    shared: Arc<Shared<S>>,
+pub struct ReadHalf {
+    shared: Arc<Shared>,
     buf: Buffer,
 }
 
-impl<S: Session> io::Read for ReadHalf<S> {
+impl io::Read for ReadHalf {
     fn read(&mut self, buf: &mut [u8]) -> io::Result<usize> {
-        let mut session = self.shared.session.lock().unwrap();
+        let mut connection = self.shared.connection.lock().unwrap();
 
-        while session.wants_read() {
+        while connection.wants_read() {
             if self.buf.is_empty() {
-                drop(session);
+                drop(connection);
 
                 let bytes_read = self.buf.read_from(&mut &self.shared.stream)?;
 
-                session = self.shared.session.lock().unwrap();
+                connection = self.shared.connection.lock().unwrap();
 
                 if bytes_read == 0 {
                     break;
                 }
             }
 
-            let bytes_read = session.read_tls(&mut self.buf)?;
+            let bytes_read = connection.read_tls(&mut self.buf)?;
             debug_assert_ne!(bytes_read, 0);
 
-            session
+            connection
                 .process_new_packets()
                 .map_err(|e| io::Error::new(io::ErrorKind::InvalidData, e))?;
         }
 
-        match session.read(buf) {
+        match connection.reader().read(buf) {
             Ok(0) => Err(io::Error::new(
                 io::ErrorKind::UnexpectedEof,
                 "TLS connection closed improperly",
@@ -57,60 +58,60 @@ impl<S: Session> io::Read for ReadHalf<S> {
     }
 }
 
-impl<S: Session> ReadHalf<S> {
+impl ReadHalf {
     pub fn shutdown(&mut self, how: Shutdown) -> io::Result<()> {
         self.shared.stream.shutdown(how)
     }
 }
 
-pub struct WriteHalf<S: Session> {
-    shared: Arc<Shared<S>>,
+pub struct WriteHalf {
+    shared: Arc<Shared>,
     buf: Buffer,
 }
 
-impl<S: Session> WriteHalf<S> {
+impl WriteHalf {
     pub fn shutdown(&mut self, how: Shutdown) -> io::Result<()> {
         if how == Shutdown::Read {
             return self.shared.stream.shutdown(Shutdown::Read);
         }
 
-        let mut session = self.shared.session.lock().unwrap();
-        session.send_close_notify();
-        let res = flush(&mut self.buf, &self.shared, session);
+        let mut connection = self.shared.connection.lock().unwrap();
+        connection.send_close_notify();
+        let res = flush(&mut self.buf, &self.shared, connection);
         self.shared.stream.shutdown(how)?;
         res
     }
 }
 
-fn wants_write_loop<'a, S: Session>(
+fn wants_write_loop<'a>(
     buf: &mut Buffer,
-    shared: &'a Shared<S>,
-    mut session: MutexGuard<'a, S>,
-) -> io::Result<MutexGuard<'a, S>> {
-    while session.wants_write() {
+    shared: &'a Shared,
+    mut connection: MutexGuard<'a, Connection>,
+) -> io::Result<MutexGuard<'a, Connection>> {
+    while connection.wants_write() {
         while buf.is_full() {
-            drop(session);
+            drop(connection);
 
             buf.write_to(&mut &shared.stream)?;
 
-            session = shared.session.lock().unwrap();
+            connection = shared.connection.lock().unwrap();
         }
 
-        session.write_tls(buf)?;
+        connection.write_tls(buf)?;
     }
 
-    Ok(session)
+    Ok(connection)
 }
 
-fn flush<'a, S: Session>(
+fn flush<'a>(
     buf: &mut Buffer,
-    shared: &'a Shared<S>,
-    mut session: MutexGuard<'a, S>,
+    shared: &'a Shared,
+    mut connection: MutexGuard<'a, Connection>,
 ) -> io::Result<()> {
-    session.flush()?;
+    connection.writer().flush()?;
 
-    let session = wants_write_loop(buf, shared, session)?;
-    std::mem::drop(session);
+    let connection = wants_write_loop(buf, shared, connection)?;
+    std::mem::drop(connection);
 
     while !buf.is_empty() {
         buf.write_to(&mut &shared.stream)?;
@@ -119,30 +120,30 @@ fn flush<'a, S: Session>(
     Ok(())
 }
 
-impl<S: Session> io::Write for WriteHalf<S> {
+impl io::Write for WriteHalf {
     fn write(&mut self, buf: &[u8]) -> io::Result<usize> {
-        let session = self.shared.session.lock().unwrap();
-        let mut session = wants_write_loop(&mut self.buf, &self.shared, session)?;
-        session.write(buf)
+        let connection = self.shared.connection.lock().unwrap();
+        let mut connection = wants_write_loop(&mut self.buf, &self.shared, connection)?;
+        connection.writer().write(buf)
     }
 
     fn flush(&mut self) -> io::Result<()> {
-        let session = self.shared.session.lock().unwrap();
-        flush(&mut self.buf, &self.shared, session)
+        let connection = self.shared.connection.lock().unwrap();
+        flush(&mut self.buf, &self.shared, connection)
     }
 }
 
-pub fn split<S: Session, D1: Into<Vec<u8>>, D2: Into<Vec<u8>>>(
+pub fn split<D1: Into<Vec<u8>>, D2: Into<Vec<u8>>>(
     stream: TcpStream,
-    session: S,
+    connection: Connection,
     read_buf_cfg: BufCfg<D1>,
     write_buf_cfg: BufCfg<D2>,
-) -> (ReadHalf<S>, WriteHalf<S>) {
-    assert!(!session.is_handshaking());
+) -> (ReadHalf, WriteHalf) {
+    assert!(!connection.is_handshaking());
 
     let shared = Arc::new(Shared {
         stream,
-        session: Mutex::new(session),
+        connection: Mutex::new(connection),
     });
 
     let read_half = ReadHalf {
